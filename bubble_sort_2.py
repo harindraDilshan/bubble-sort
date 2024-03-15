247ef746d5aee9cf701a6e422bba70b64d08fc78
harindraDilshan <osah.dilshan@gmail.com>
2024-03-15 20:35:53 +0530

    Revert "I wrote some comment"
    This reverts commit 98df3896ad16a41be0df98e48dd7b0efb0bf1968.

--- bubble_sort_2.py
+++ bubble_sort_2.py
@@ -5,7 +5,7 @@ def bubble_sort(arr):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
-# I changed this code. bubble_sort_3.py comming soon ...
+        
 arr = [12, 6, 7, 8, 10, 2, 9]
 
 print(bubble_sort(arr))
