b47b643bd9b34667067a94162450fa1b5606592e
harindraDilshan <osah.dilshan@gmail.com>
2024-03-15 17:21:45 +0530

    Initial Commit

---
+++ bubble_sort_2.py
@@ -0,0 +1,11 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(0, n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+        
+arr = [12, 6, 7, 8, 10, 2, 9]
+
+print(bubble_sort(arr))
